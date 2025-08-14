931499a01b3477e71dcd8616dc4b8b4c07086ef1
trungkin <trungkien.nd03@gmail.com>
2025-08-15 00:30:37 +0700

    Effect Pause Done, Next Transition All Tab

--- Assets/_Assets/Scripts/UI/PauseController.cs
+++ Assets/_Assets/Scripts/UI/PauseController.cs
@@ -1,13 +1,12 @@
 using DG.Tweening;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PauseController : MonoBehaviour
 {
     [Header("Screen")]
-    [SerializeField] GameObject testButton;
+    [SerializeField] GameObject _buttonResume;
 
     [Header("ShowHand")]
     [SerializeField] GameObject _handObj;
@@ -17,20 +16,38 @@ public class PauseController : MonoBehaviour
     private Vector2 _hiddenPos = new Vector2(1140f, 145f);
     private Vector2 _showPos = new Vector2(182f, 145f);
     private bool _isShown = false;
+    private Material _materialHand;
+  
+    private float _matDurationON = 0.1f;
+    private float _matDurationOFF = 0.2f;
 
-    private Material _material;
+    [Header("ShowTablet")]
+    [SerializeField] GameObject _borderObj;
+    private Image _imageBorder;
+    private Material _materialImageBorder;
 
+    [Header("ShowContent")]
+    [SerializeField] GameObject _contentTablet;
+    public int _blinkCount = 2;
+    public float _blinkInterval = 0.1f;
+    
+    // IntensityX
     private string _para_NAME_IntensityX = "_IntensityX";
     private string _para_NAME_IntensityY = "_IntensityY";
     private float _paraIntensityStart = 0f;
     private float _paraIntensityTargetX = 0.008f;
     private float _paraIntensityTargetY = 0.008f;
-    private float _matDurationON = 0.1f;
-    private float _matDurationOFF = 0.2f;
 
+    // Reveal
+    private float _para_RevealStart = 1f;
+    private float _para_RevealEnd = 0f;
+    private static string _para_NAME_REVEAL = "_Reveal";
+    private float _revelDuration = 0.1f;
     private void Start()
     {
+        _contentTablet.SetActive(false);
         SetupHandObj();
+        SetupReveal();
     }
 
     private void OnEnable()
@@ -50,12 +67,20 @@ public class PauseController : MonoBehaviour
         _handTransform = _handObj.GetComponent<RectTransform>();
         _handImage = _handTransform.GetComponent<Image>();
 
-        _material = Instantiate(_handImage.material);
-        _handImage.material = _material;
+        _materialHand = Instantiate(_handImage.material);
+        _handImage.material = _materialHand;
 
         _handTransform.anchoredPosition = _hiddenPos;
-        _material.SetFloat(_para_NAME_IntensityX, _paraIntensityStart);
-        _material.SetFloat(_para_NAME_IntensityY, _paraIntensityStart);
+        _materialHand.SetFloat(_para_NAME_IntensityX, _paraIntensityStart);
+        _materialHand.SetFloat(_para_NAME_IntensityY, _paraIntensityStart);
+    }
+
+    private void SetupReveal()
+    {
+        _imageBorder = _borderObj.GetComponent<Image>();
+        _materialImageBorder = Instantiate(_imageBorder.material);
+        _imageBorder.material = _materialImageBorder;
+        _materialImageBorder.SetFloat(_para_NAME_REVEAL, _para_RevealStart);
     }
 
     private void OnScreen()
@@ -70,19 +95,34 @@ public class PauseController : MonoBehaviour
 
     private IEnumerator HandON()
     {
-        testButton.SetActive(true);
+        _buttonResume.SetActive(true);
+        
+        // ChangePosition
         yield return StartCoroutine(MoveHand());
-        yield return StartCoroutine(ChangMatHand(_material.GetFloat(_para_NAME_IntensityX), _paraIntensityTargetX, _para_NAME_IntensityX, _matDurationON));
-        yield return StartCoroutine(ChangMatHand(_material.GetFloat(_para_NAME_IntensityY), _paraIntensityTargetY, _para_NAME_IntensityY, _matDurationON));
 
+        // effect 3D
+        StartCoroutine(ChangeMat( _materialHand, _materialHand.GetFloat(_para_NAME_IntensityX), _paraIntensityTargetX, _para_NAME_IntensityX, _matDurationON));
+        StartCoroutine(ChangeMat( _materialHand, _materialHand.GetFloat(_para_NAME_IntensityY), _paraIntensityTargetY, _para_NAME_IntensityY, _matDurationON));
+
+        // effect Open tablet 
+        yield return StartCoroutine(ChangeMat( _materialImageBorder, _materialImageBorder.GetFloat(_para_NAME_REVEAL), _para_RevealEnd, _para_NAME_REVEAL, _revelDuration));
+        StartCoroutine(ChangeMat(_materialImageBorder, _materialImageBorder.GetFloat(_para_NAME_IntensityX), _paraIntensityTargetX, _para_NAME_IntensityX, _matDurationON));
+        StartCoroutine(ChangeMat(_materialImageBorder, _materialImageBorder.GetFloat(_para_NAME_IntensityY), _paraIntensityTargetY, _para_NAME_IntensityY, _matDurationON));
+
+        // effect blink tablet
+        StartCoroutine(BlinkBeforeOn(_contentTablet, _blinkCount, _blinkInterval));
     }
 
     private IEnumerator HandOFF()
     {
-        testButton.SetActive(false);
-        yield return StartCoroutine(ChangMatHand(_material.GetFloat(_para_NAME_IntensityX), _paraIntensityStart, _para_NAME_IntensityX, _matDurationOFF));
-        yield return StartCoroutine(ChangMatHand(_material.GetFloat(_para_NAME_IntensityY), _paraIntensityStart, _para_NAME_IntensityY, _matDurationOFF));
+        _buttonResume.SetActive(false);
+        _contentTablet.SetActive(false);
+        yield return StartCoroutine(ChangeMat(_materialImageBorder, _materialImageBorder.GetFloat(_para_NAME_REVEAL), _para_RevealStart, _para_NAME_REVEAL, _revelDuration));
+        yield return StartCoroutine(ChangeMat(_materialHand, _materialHand.GetFloat(_para_NAME_IntensityX), _paraIntensityStart, _para_NAME_IntensityX, _matDurationOFF));
+        yield return StartCoroutine(ChangeMat(_materialHand, _materialHand.GetFloat(_para_NAME_IntensityY), _paraIntensityStart, _para_NAME_IntensityY, _matDurationOFF));
         yield return StartCoroutine(MoveHand());
+        MatReset(_materialImageBorder, _para_NAME_IntensityX, _paraIntensityStart);
+        MatReset(_materialImageBorder, _para_NAME_IntensityY, _paraIntensityStart);
     }
 
     private IEnumerator MoveHand()
@@ -96,15 +136,31 @@ public class PauseController : MonoBehaviour
         yield return t.WaitForCompletion();
     }
 
-    private IEnumerator ChangMatHand(float start, float target, string name, float duration)
+    private IEnumerator ChangeMat( Material material, float start, float target, string name, float duration)
     {
         float timer = 0f;
         while (timer < duration)
         {
             timer += Time.unscaledDeltaTime;
             float value = Mathf.Lerp(start, target, timer / duration);
-            _material.SetFloat(name, value);
+            material.SetFloat(name, value);
             yield return null;
         }
+    }
+
+    private IEnumerator BlinkBeforeOn(GameObject obj, int blinkCount, float blinkTime)
+    {
+        for(int i = 0; i < blinkCount; i++)
+        {
+            obj.SetActive(!obj.activeSelf);
+            yield return new WaitForSecondsRealtime(blinkTime);
+        }    
+
+        obj.SetActive(true);
+    }    
+
+    private void MatReset( Material material, string name, float value)
+    {
+        material.SetFloat(name, value);
     }
 }
