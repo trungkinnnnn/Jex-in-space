d9a435ee8c75d4dddeec1b4df9c256facbee2972
trungkin <trungkien.nd03@gmail.com>
2025-08-17 16:20:25 +0700

    Done Screen Die

--- Assets/_Assets/Scripts/UI/Screen/ShopGunScreenUI.cs
+++ Assets/_Assets/Scripts/UI/Screen/ShopGunScreenUI.cs
@@ -1,43 +1,113 @@
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class ShopGunScreenUI : MonoBehaviour
 {
+
+    public Action<int> actionSelected;
     [Header("Border")]
-    [SerializeField] List<GameObject> _borderSelected;
+    [SerializeField] List<GameObject> _borderSelectedObj;
 
     [Header("Lock")]
-    [SerializeField] List<GameObject> _imageLock;
+    [SerializeField] List<Image> _imageLock;
 
     [Header("Unlock")]
-    [SerializeField] List<GameObject> _imageUnlock;
+    [SerializeField] List<Image> _imageUnlock;
 
-    private List<GunSelected> _gunSelecteds;
+    private List<Image> _imageList = new List<Image>();
+    private List<Button> _buttons = new List<Button>();
 
-    private void Start()   {
-        
+    private int _itemCount;
+    private int _currentSelected = 0;
+    private float _durationChangeAlpha = 0.3f;
+    private float _alphaMax = 1f;
+    private float _alphaMin = 0f;
+    private void Start()   
+    {
+        SetUp();
+        AddListener();
+        actionSelected += HandleSelectedGun;
     }
 
-    private void LoadData()
+    private void SetUp()
     {
-        var count = _borderSelected.Count;
-        if (count != _imageLock.Count || count != _imageUnlock.Count)
+        _itemCount = _borderSelectedObj.Count;
+        if (_itemCount != _imageLock.Count || _itemCount != _imageUnlock.Count)
         {
             Debug.Log("Data GunSelected Not Valid");
             return;
         }
 
+        for(int i = 0; i < _itemCount; i++)
+        {
+            Image image = _borderSelectedObj[i].GetComponent<Image>();
+            Button button = _borderSelectedObj[i].GetComponent<Button>();
+            _imageList.Add(image);
+            _buttons.Add(button);
+        }
 
     }
 
 
-}
+    private void AddListener()
+    {
+        
 
-[System.Serializable]
-public class GunSelected
-{
-    public GameObject imageBorderSelected;
-    public GameObject imageLock;
-    public GameObject imageUnlock;
+        for(int i = 0; i< _itemCount; i++)
+        {
+            int index = i;
+            _buttons[i].onClick.AddListener(() => OnClick(index));
+        }    
+
+    }
+
+    private void OnClick(int index)
+    {
+        Debug.Log("Gun :" +  index + "Current : " + _currentSelected);
+        if (index == _currentSelected) return;
+        actionSelected?.Invoke(index);
+    }    
+
+    private void HandleSelectedGun(int index)
+    {
+        for (int i = 0; i < _itemCount; i++)
+        {
+            if (index != i) continue;
+            Image imageSelected = _imageList[index];
+            Image imageCurrent = _imageList[_currentSelected];
+            _currentSelected = i;
+            OnSelected(imageSelected, imageCurrent);
+            break;
+        }
+    }
+
+    private void OnSelected(Image imageSelected, Image imageCurrent)
+    {
+        StartCoroutine(ChangeAlpha(imageSelected, _alphaMin, _alphaMax));
+        StartCoroutine(ChangeAlpha(imageCurrent, _alphaMax, _alphaMin));
+    }    
+
+    private IEnumerator ChangeAlpha(Image image, float start, float end)
+    {
+        float timer = 0f;
+        while(timer <= _durationChangeAlpha)
+        {
+            timer += Time.unscaledDeltaTime;
+            float alpha = Mathf.Lerp(start, end, timer/_durationChangeAlpha);
+            SetAlpha(image, alpha);
+            yield return null;
+        }
+    }
+
+    private void SetAlpha(Image image, float alpha)
+    {
+        Color color = image.color;
+        color.a = alpha;
+        image.color = color;
+    }
+
 }
